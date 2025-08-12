6cb2c2b744663819b147d4cc1617e469851bd816
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2025-08-12 14:04:42 +0100

    Remove Joker from main menu
    was causing crashes on new save creation

--- KHJokers.lua
+++ KHJokers.lua
@@ -43,31 +43,6 @@ SMODS.Atlas({
   atlas_table = 'ANIMATION_ATLAS'
 })
 
--- adds Kingdom Hearts Joker to the title menu
-function Card:resize(mod, force_save)
-  self:hard_set_T(self.T.x, self.T.y, self.T.w * mod, self.T.h * mod)
-  remove_all(self.children)
-  self.children = {}
-  self.children.shadow = Moveable(0, 0, 0, 0)
-  self:set_sprites(self.config.center, self.base.id and self.config.card)
-end
-
-local mainmenuref2 = Game.main_menu
-Game.main_menu = function(change_context)
-  local ret = mainmenuref2(change_context)
-
-  local newcard = SMODS.create_card({ key = 'j_kh_khtrilogy', area = G.title_top })
-  G.title_top.T.w = G.title_top.T.w * 1.7675
-  G.title_top.T.x = G.title_top.T.x - 0.8
-  G.title_top:emplace(newcard)
-  newcard:start_materialize()
-  newcard:resize(1.1 * 1.2)
-  newcard.no_ui = true
-  return ret
-end
-
-
-
 KH = SMODS.current_mod
 
 KH.save_config = function(self)
